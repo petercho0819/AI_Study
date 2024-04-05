6077e1b035c9d8c0f48e2767b78089d339f8c7d1
petercho0819 <dongr52@naver.com>
2024-04-05 09:43:35 +0900

    경사 하강법 복습 및 주석 처리

--- 3주차/과제/back_propagation.py
+++ 3주차/과제/back_propagation.py
@@ -1,10 +1,13 @@
-# gradient desent 
+# gradient descent 
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 X = np.random.rand(100)
-# print(x)
-Y = 0.2 * X * 0.5
+print(X)
+# 임의의 f(x) 정의
+# y = wx + b <= 1차 방정식
+Y = 0.2 * X + 0.5
 plt.figure(figsize=(8,6))
 plt.scatter(X,Y)
 plt.show()
@@ -12,22 +15,28 @@ plt.show()
 
 def plot_prediction(pred,y) : 
     plt.figure(figsize=(8,6))
+# 실제값을 보여줌
     plt.scatter(X, y)
+# 예측 값을 보여줌
     plt.scatter(X,pred)
     plt.show()
 
 ## gradient Descent 구현
+#.  -1 ~ 1까지  랜덤 수
 # 업데이트 할 W : Learning Rate * ((Y예측 - Y실제) * X) 평균
 # 업데이트 할 b : Learning Rate * ((Y예측 - Y실제) * 1) 평균
 W = np.random.uniform(-1, 1)
 b = np.random.uniform(-1, 1)
-
+print(W) 
+print(b)
 learning_rate = 0.7
 
 for epoch in range(200):
     Y_Pred = W * X + b
 
+# 예측값 - 실제값의 절대값의 평균
     error = np.abs(Y_Pred - Y).mean()
+    # 에러 잡는 이유 : 에러가 어느 정도 도달하면 멈추기 위해서
     if error < 0.001:
         break
 
@@ -36,9 +45,12 @@ for epoch in range(200):
     b_grad = learning_rate * ((Y_Pred - Y) * 1).mean()
 
     # W, b 값 갱신
+    # !질문 : 왜 다시 갱신을 해줄까
     W = W - w_grad
     b = b - b_grad
 
     if epoch % 10 == 0:
         Y_Pred = W * X + b
         plot_prediction(Y_Pred, Y)
+
+# 최소 제곱법
---
+++ 3주차/과제/my_perceptron.py
@@ -0,0 +1,82 @@
+# gradient descent 
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+from IPython.display import clear_output
+
+# 계단 함수 정의
+# 역치보다 크면 1 아니면 0; 여기서의 역치는 0.5로 지정
+def step_function(x) :
+    if x < thres:
+        return 0
+    else :
+        return 1
+
+# 랜덤으로 데이터를 만들어주는 function
+def gen_training_data(data_point):
+    # 0 ~ 1까지 랜덤으로 생성해줌
+    x1 = np.random.random(data_point)
+    x2 = np.random.random(data_point)
+    # x1 + x2가 1보다 크면 1 아니면 0
+    y = ((x1 + x2) > 1).astype(int)
+    # 하나의 데이터이고 이것을 100개로 만들 예정
+    training_set = [((x1[i], x2[i]), y[i]) for i in range(len(x1))]
+
+    return training_set
+# 역치
+thres = 0.5
+w = np.array([0.3, 0.9])
+# learing rate
+lr = 0.1
+data_point = 100
+# 데이터를 얼마나 반복적으로 할 것인가
+epoch = 10
+training_set = gen_training_data(data_point)
+# print(training_set[0:5])
+plt.figure(0)
+plt.ylim(-0.1, 1.1)
+plt.xlim(-0.1, 1.1)
+ax = plt.gca()
+ax.set_aspect('equal', adjustable='box')
+
+for x, y in training_set:
+    if y == 1:
+        plt.plot(x[0],x[1], 'bo')
+    else:
+        plt.plot(x[0],x[1], 'go')
+plt.show()
+
+# 애니메이션 처럼 보이기 위한 것
+# %matplotlib inline
+from time import sleep
+plt.figure(0)
+plt.ylim(-0.1, 1.1)
+plt.xlim(-0.1, 1.1)
+ax = plt.gca()
+ax.set_aspect('equal', adjustable='box')
+
+xx = np.linspace(0,1,50)
+for i in range(epoch):
+    cnt = 0
+    for x , y in training_set:
+        clear_output(wait=True)
+        u = sum(x * w)
+        error = y - step_function(u)
+        for index, value in enumerate(x):
+            w[index] = w[index] + lr * error * value
+        for xs, ys in training_set[0:cnt]:
+            plt.ylim(-0.1,1.1)
+            plt.xlim(-0.1,1.1)
+            ax = plt.gca()
+            ax.set_aspect('equal', adjustable='box')
+
+            if ys == 1:
+                plt.plot(xs[0], xs[1], 'bo')
+            else : 
+                plt.plot(xs[0], xs[1], 'go')
+        
+        yy = -w[1]/w[0] * xx + thres /w[0] # <== w[0] * yy + w[1] * xx = thres
+        plt.plot(xx, yy)
+        plt.show()
+        cnt = cnt+ 1
+        sleep(0.01)
