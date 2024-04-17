1f6aeeb8b99a831d774839a3e11e57e0c48eea5d
petercho0819 <dongr52@naver.com>
2024-04-17 22:28:10 +0900

    240417 daily commit

--- 3주차/과제/backpropagation.py
+++ 3주차/과제/backpropagation.py
@@ -20,4 +20,30 @@ class MLP:
     def forward(self, x):
 
         self.z1_2 = np.dot(x, self.w1_2_3_4)
-        
+        self.h = self.sigmoid(self.z1_2)
+        self.z3 = np.dot(self.h, self.w5_6)
+        self.o = self.sigmoid(self.z3)
+        return self.o
+    
+    # 평균 loss 계산
+    def mse_loss(self, y_true,  y_pred):
+        return np.mean((y_true - y_pred) ** 2)
+    
+    def backward(self, x, y, y_pred, learning_rate):
+        dc_do1 = -2 * (y - y_pred)
+        do1_dz3 = y_pred * (1 - y_pred)
+        dz3_dw5_6 = self.h
+        dc_dw5_6 = dc_do1 * do1_dz3 * dz3_dw5_6
+        self.w5_6 = self.w5_6 + learning_rate * -dc_dw5_6.T
+        dc_dw1_2_3_4 = dc_do1 * do1_dz3 * np.dot(self.w5_6 * (self.h * (1- self.h)).T, x)
+
+        self.w1_2_3_4 = self.w1_2_3_4 + learning_rate * -dc_dw1_2_3_4.T
+
+    def train (self, X_train, y_train, epochs, learning_rate):
+        for epoch in range(epochs) :
+            for i in range(len(X_train)):
+                y_pred = self.forward([X_train[i]])
+                loss = self.mse_loss([y_train[i]], y_pred)
+                self.backward([X_train[i]], [y_train[i]], y_pred, learning_rate)
+                if np.mod(epoch, 100) ==0:
+                    print('epoch = ', epoch, 'loss = ', loss)
