50e6b926a2c3eacbf024c1a220675a6b2116aab9
petercho0819 <dongr52@naver.com>
2024-05-06 17:41:53 +0900

    240506 daily commit

--- 3주차/과제/backpropagation.py
+++ 3주차/과제/backpropagation.py
@@ -9,11 +9,11 @@ class MLP:
 
         # 가중치 초기화
         # 보통은 랜덤하게 선언할 수 있지만 이번 예제의 경우는 원할한 학습 과정을 위해서 초기값을 fix
-        # self.w1_2_3_4 = np.random.random((self.input_size, self.hidden_size))
-        self.w1_2_3_4 = [[1,10],[1,10]]
+        self.w1_2_3_4 = np.random.random((self.input_size, self.hidden_size))
+        # self.w1_2_3_4 = [[1,10],[1,10]]
 
-        # self.w5_6 =  np.random.random((self.hidden_size, self.output_size))
-        self.w5_6 =  [[-40],[40]]
+        self.w5_6 =  np.random.random((self.hidden_size, self.output_size))
+        # self.w5_6 =  [[-40],[40]]
 
     def sigmoid(self, x):
         return 1 / (1 + np.exp(-x))
@@ -51,15 +51,21 @@ class MLP:
     def train (self, X_train, y_train, epochs, learning_rate):
         for epoch in range(epochs) :
             for i in range(len(X_train)):
+                # forward pass
                 y_pred = self.forward([X_train[i]])
+                # compute and print loss
                 loss = self.mse_loss([y_train[i]], y_pred)
+                # backward pass
                 self.backward([X_train[i]], [y_train[i]], y_pred, learning_rate)
-                if np.mod(epoch, 100) ==0:
-                    print('epoch = ', epoch, 'loss = ', loss)
+            if np.mod(epoch, 100) ==0:
+                print('epoch = ', epoch, 'loss = ', loss)
 
+# 데이터 생성 (100개)
 x_train = np.random.randint(0,2,(100,2))
 y_train = (x_train[:,0]!=x_train[:,1].astype(int))
+# 다층퍼셉트론 선언
 mlp = MLP(input_size=2, hidden_size=2, output_size=1)
+# 모델 학습
 mlp.train(x_train, y_train, epochs=1000, learning_rate=0.1)
 
 test_input = np.array([[0,0]])
